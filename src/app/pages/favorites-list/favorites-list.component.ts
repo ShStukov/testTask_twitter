e8ef2f995bc0a2efd9f64f53eec8de03de73cd65
Alexandr Stukov <227stukov1337@gmail.com>
2025-06-23 10:51:27 +0300

    change favorites-list

--- src/app/pages/favorites-list/favorites-list.component.ts
+++ src/app/pages/favorites-list/favorites-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { PostWithDetails } from '../../data/models/post-with-details.models';
 import { Router, RouterModule } from '@angular/router';
 import { FavoritesService } from '../../data/services/favorites.service';
-import { catchError, of, finalize, distinctUntilChanged } from 'rxjs';
+import { catchError, of, finalize, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { PostCardComponent } from '../user-posts/post-card/post-card.component';
 import { PostDetailsService } from '../../data/services/post-details.service';
@@ -24,6 +24,8 @@ export class FavoritesListComponent {
   loading: boolean = true;
   error: string | null = null;
 
+  private destroy$ = new Subject<void>();
+
   constructor(
     private router: Router,
     private favoritesService: FavoritesService,
@@ -33,10 +35,15 @@ export class FavoritesListComponent {
   ngOnInit(): void {
     this.favoritesService.favoritePostIds$
       .pipe(
-        takeUntilDestroyed(),
+        takeUntil(this.destroy$),
         distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
       )
       .subscribe(() => this.loadFavoritePosts());
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   private loadFavoritePosts(): void {
